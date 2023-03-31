8132658addeb13997ab6b79d9ad09f49a06f35e3
fujitoo <fabiobentaila@gmail.com>
2023-03-31 23:11:55 +0200

    Updates to README and database export

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 Backend d'une solution de gestion d'articles de blogs.
 Un export de notre base de données est disponible dans le dossier Resources situé à la racine du projet.
 
+## URL d'accès à notre Frontend
+
+Nous avons un client permettant de s'identifier, de consulter les articles et d'en créer.
+Il est accessible via le lien suivant : 'https://blogfi.faister.fr/index.php/v1/Api/Client/login/'
+
 ## URLs d'accès à notre backend
 
 Notre projet étant publié en ligne, il y a deux manières d'accèder à notre backend:
--- Resources/u563109936_Blog_FI.sql
+++ Resources/u563109936_Blog_FI.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 31 mars 2023 à 19:54
+-- Généré le : ven. 31 mars 2023 à 21:06
 -- Version du serveur : 10.6.10-MariaDB-cll-lve
 -- Version de PHP : 7.2.34
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Articles` (
   `IdArticle` int(11) NOT NULL,
-  `Title` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Content` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `DateModif` date DEFAULT NULL,
   `DatePub` date NOT NULL,
@@ -41,11 +41,16 @@ CREATE TABLE `Articles` (
 --
 
 INSERT INTO `Articles` (`IdArticle`, `Title`, `Content`, `DateModif`, `DatePub`, `IdUser`) VALUES
-(1, 'Top 5 des Langages de programmation en 2023', 'L’institute of Electrical and Electronics Enginees (IEEE) a publié son classement annuel pour déterminer les meilleurs langages de programmation. Il est temps d’y jeter un œil pour avoir une idée du nouveau langage de programmation sur lequel se former cette nouvelle année. En 2021, Python arrivait en tête du classement. Cette année, c’est... Python, qui une nouvelle fois l’emporte. Explications\nAfin de mesurer la demande employeur de tel ou tel langage, l’organisation s’est basée sur les données de grands sites de recrutement tel que CareerBuilder et Dice. Il est donc important de prendre du recul par rapport aux observations de l’IEEE, car celles-ci se basent sur les offres d’emploi aux Etats-Unis uniquement, et ne reflète donc pas forcément fidèlement la demande française. Cela étant, puisque le pays a généralement une longueur d’avance sur le reste du monde en la matière, il demeure très intéressant de se pencher sur les résultats pour s’attendre aux mêmes phénomènes de croissance en France dans les années à venir', NULL, '2023-03-15', 1),
+(1, 'Top 5 des Langages de programmation en 2023', 'L’institute of Electrical and Electronics Enginees (IEEE) a publié son classement annuel pour déterminer les meilleurs langages de programmation. Il est temps d’y jeter un œil pour avoir une idée du nouveau langage de programmation sur lequel se former cette nouvelle année. En 2021, Python arrivait en tête du classement. Cette année, c’est... Python, qui une nouvelle fois l’emporte. Explications\nAfin de mesurer la demande employeur de tel ou tel langage, l’organisation s’est basée sur les données de grands sites de recrutement tel que CareerBuilder et Dice. Il est donc important de prendre du recul par rapport aux observations de l’IEEE, car celles-ci se basent sur les offres d’emploi aux Etats-Unis uniquement, et ne reflète donc pas forcément fidèlement la demande française. Cela étant, puisque le pays a généralement une longueur d’avance sur le reste du monde en la matière, il demeure très intéressant de se pencher sur les résultats pour s’attendre aux mêmes phénomènes de croissance en France dans les années à venir', NULL, '2023-03-15', 4),
 (2, 'Comment installer Git sur Ubuntu', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', NULL, '2023-03-05', 2),
 (3, 'L\'IUT Informatique', 'Texte pour affichage', '2023-03-30', '2023-03-24', 2),
 (4, 'Bonjour à tous', 'les amis', NULL, '2023-03-30', 5),
-(5, 'Test de l\'api publiée !', 'la modification marche aussi !', '2023-03-31', '2023-03-31', 2);
+(5, 'Test de l\'api publiée !', 'la modification marche aussi !', '2023-03-31', '2023-03-31', 2),
+(6, 'Pourquoi le BUT Informatique est une bonne formation', 'On y apprend le JavaScript... etc', NULL, '2023-03-31', 2),
+(7, 'test', 'mytest', NULL, '2023-03-31', 2),
+(8, 'I am a test', 'testing', NULL, '2023-03-31', 2),
+(9, 'I am another test', 'Hello there are you wondering what I am doing?', NULL, '2023-03-31', 2),
+(10, 'testing', 'dont worry this is the last one', NULL, '2023-03-31', 2);
 
 -- --------------------------------------------------------
 
@@ -65,7 +70,7 @@ CREATE TABLE `Evaluate` (
 --
 
 INSERT INTO `Evaluate` (`IdArticle`, `IdUser`, `Liked`, `Disliked`) VALUES
-(1, 1, 0, 1),
+(1, 5, 0, 1),
 (2, 2, 0, 1);
 
 -- --------------------------------------------------------
@@ -86,7 +91,7 @@ CREATE TABLE `Users` (
 --
 
 INSERT INTO `Users` (`IdUser`, `UserName`, `Password`, `Role`) VALUES
-(1, 'Fabio', 'pwdFabio123', 'Publisher'),
+(1, 'Fabio', 'pwdFabio123', 'Moderator'),
 (2, 'Ismail', 'pwsIsmail234', 'Publisher'),
 (3, 'Yael', 'pwdYael567', 'Moderator'),
 (4, 'Florent', 'pwdFlorent', 'Publisher'),
@@ -125,7 +130,7 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT pour la table `Articles`
 --
 ALTER TABLE `Articles`
-  MODIFY `IdArticle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `IdArticle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `Users`
